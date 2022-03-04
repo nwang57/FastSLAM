bf5573e15e6b56c44bf19b7aee4f9b222b13c551
Rahul Seth <57781892+sethr07@users.noreply.github.com>
2022-03-04 20:29:01 +0000

    wrapped yaw angle to [-pi,pi]

--- ekf.py
+++ ekf.py
@@ -5,17 +5,15 @@ class EKF:
     def __init__(self, init_x, init_y, init_yaw):
         self.state = np.array([[init_x], [init_y], [init_yaw]]) # initial state
         self.P = np.array([[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]]) # variance array
-        self.noise = {'velocity': 0.2, 'yaw': 0.0, 'steer': 0.05, 'lidar': 0.5}
+        self.noise = {'velocity': 0.2, 'yaw': 0.05, 'steer': 0.1, 'lidar': 0.5}
 
         self.dt = 1 #timestamp
 
     def update(self, state_vec):
         measured_x, measured_y, measured_yaw, vel, gamma = state_vec
     
-        pred_state, pred_p = self.predict_step([[vel], [gamma]])
+        pred_state, pred_p = self.predict_step([vel, gamma])
         self.state, self.P = self.update_step([measured_x, measured_y, measured_yaw], [pred_state, pred_p])
-
-        self.state[2][0] = self.process_yaw(self.state[2][0])
 
         return self.state
 
@@ -28,9 +26,10 @@ class EKF:
         p = self.P
 
         pred_state = A@state + B@control + self.get_control_noise()
+        print('State estimate before ekf =', pred_state)
 
         pred_p = A@p@np.transpose(A) + abs(self.get_control_noise()) #pred state covarince
-
+        
         return pred_state, pred_p
 
     def update_step(self, measurement, pred):
@@ -46,8 +45,12 @@ class EKF:
 
         S_k = H@pred_p@np.transpose(H) + abs(self.get_update_noise())
 
+        print("S_k : ", S_k)
+        print("det(S_k) : ", np.linalg.det(S_k))
+
         K = pred_p@np.transpose(H)@np.linalg.pinv(S_k)
 
+        print("K : ", K)
         update_state = pred_state + K@y_k
         update_p = (np.identity(3) - K@H)@pred_p
 
@@ -76,9 +79,10 @@ class EKF:
         return np.array(vec)
 
     def process_yaw(self, yaw):
-        pi = 3.14159
-        if yaw > pi and yaw < pi*2:
+        pi = np.pi
+        yaw = yaw % (2 * pi)
+        if yaw > pi: 
             return yaw - 2*pi
-        elif yaw > 2*pi:
-            return yaw - 2*pi
+        elif yaw < 2*pi:
+            return yaw + 2*pi
         return yaw
